89b8a02f3fcdc017b60fbd0a08822a1e5b54cf07
majikarikeruo <castero1219@gmail.com>
2024-11-11 15:17:12 +0900

    [add]1110時点の素を添付

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\IconController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('test');
-});
+Route::get('/dashboard', [IconController::class, 'index']);
+Route::get('/icon/create', [IconController::class, 'create']);
+Route::get('/icon/{icon_id}', [IconController::class, 'show']);
 
+// 登録用のルート設定の書き方の基本
+Route::post('/icon/store', [IconController::class, 'store']);
